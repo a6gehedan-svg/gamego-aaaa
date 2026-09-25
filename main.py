3e8378dbf7687d8c6d002158eb1a105b39dad617
a6gehedan-svg <a6gehedan@gmail.com>
2026-09-25 21:59:34 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 mySprite = sprites.create(img("""
-        . . . . 3 . . 3 3 3 . . . . . .
-        . . . . . . 3 3 . 3 . . . . . .
-        . . 3 3 . 3 . . . 3 . . . . . .
-        . . . 3 3 3 . . . 3 3 . . . . .
-        . . . . 3 3 3 . . 3 3 . . . . .
-        . . . . 3 . . 3 3 3 . . . . . .
-        . . . . 3 . . . 3 3 3 . . . . .
-        . . . . 3 3 . 3 . . 3 . . . . .
-        . . . . . 3 3 3 3 3 . . . . . .
-        . . . . . 3 3 . . . . . . . . .
-        . . . . 3 3 . . . . . . . . . .
-        . . . . 3 . . . . . . . . . . .
-        . . . . 3 3 3 3 3 3 3 3 3 3 3 3
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
+        2 . . . . . . . . . . . . . . 2
+        . 2 . . . . . . . . . . . . 2 .
+        . . 2 . . . . . . . . . . 2 . .
+        . . . 2 . . . . . . . . 2 . . .
+        . . . . 2 . . . . . . 2 . . . .
+        . . . . . 2 . . . . 2 . . . . .
+        . . . . . . 2 . . 2 . . . . . .
+        . . . . . . . 2 2 . . . . . . .
+        . . . . . . . 2 2 . . . . . . .
+        . . . . . . 2 . . 2 . . . . . .
+        . . . . . 2 . . . . 2 . . . . .
+        . . . . 2 . . . . . . 2 . . . .
+        . . . 2 . . . . . . . . 2 . . .
+        . . 2 . . . . . . . . . . 2 . .
+        . 2 . . . . . . . . . . . . 2 .
+        2 . . . . . . . . . . . . . . 2
         """),
     SpriteKind.player)
-picture = image.screen_image()
-picture.fill(0)
+picture = image.create(scene.screen_width(), scene.screen_height())
+picture.fill(2)
 scene.set_background_image(picture)
-index = 0
-while index <= image.get_dimension(picture, image.Dimension.WIDTH):
-    index2 = 0
-    while index2 <= image.get_dimension(picture, image.Dimension.HEIGHT):
-        picture.set_pixel(0, 0, 0)
-        index2 += 1
-    index += 1
+i = 0
+while i <= image.get_dimension(picture, image.Dimension.WIDTH):
+    j = 0
+    while j <= image.get_dimension(picture, image.Dimension.HEIGHT):
+        picture.set_pixel(i, j, (i + j) / 3)
+        scene.set_background_image(picture)
+        print(picture)
+        j += 1
+    pause(0)
+    i += 1
